b5aa261fbdae8f9be38850cff9bff9d73e0e6bf3
Dennis Coldwd <coldwd@muir.local>
2015-09-03 10:50:11 -0700

    adding directory initialization

--- install.sh
+++ install.sh
@@ -33,15 +33,22 @@ OS_TYPE=`uname`
 # Cleanup
 # - removes any pre-installed dotfiles
 #-----------------------------------------------------------------------------------
-echo "Cleaning any previous dotfiles..."
+echo "- Cleaning any previous dotfiles"
 rm -f $HOME/.vimrc; rm -rf $HOME/.vim;
 rm -f $HOME/.profile; rm -f $HOME/.bashrc;
+
+#-----------------------------------------------------------------------------------
+# Directory Initialization
+# - sets up our required directories
+#-----------------------------------------------------------------------------------
+echo "- Creating initial directories"
+mkdir -p $HOME/src $HOME/bin
 
 #-----------------------------------------------------------------------------------
 # bash
 # -installs .profile
 #-----------------------------------------------------------------------------------
-echo "Installing .profile"
+echo "- Installing .profile"
 cp -p bash/profile $HOME/.profile
 ln -s $HOME/.profile $HOME/.bashrc
 
@@ -49,7 +56,7 @@ ln -s $HOME/.profile $HOME/.bashrc
 # vim
 # - installs vim dotfiles & associated plugins
 #-----------------------------------------------------------------------------------
-echo "Installing vim dotfiles..."
+echo "- Installing vim dotfiles"
 
 # Mac OSX has vim 7.3 installed by default, as a simple fix, we install MacVim
 # and symlink vim to our local bin directory. This gets us the latest & greatest
