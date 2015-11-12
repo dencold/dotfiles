c6734e83253af33d586437c8b00d905b2dfa4184
Dennis Coldwell <dennis.coldwell@gmail.com>
2015-11-12 10:57:41 -0800

    getting the actual file path for os-specific install scripts

--- osx/install.sh
+++ osx/install.sh
@@ -28,11 +28,17 @@ mkdir -p $HOME/src $HOME/bin
 # bash
 # -installs .profile
 #-----------------------------------------------------------------------------------
+
+# first, figure out what directory we are in. This script is called from the main 
+# install.sh in the root directory (dotfiles). This SO article is *very* helpful:
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 echo "- Installing .profile"
-cp -p .bashrc $HOME/.bashrc
-cp -p .bash_profile $HOME/.bash_profile
-cp -p .inputrc $HOME/.inputrc
+cp -p $DIR/.bashrc $HOME/.bashrc
+cp -p $DIR/.bash_profile $HOME/.bash_profile
+cp -p $DIR/.inputrc $HOME/.inputrc
 
 # finally, copy .vimrc
-cp -p .vimrc $HOME/.vimrc
+cp -p $DIR/.vimrc $HOME/.vimrc
 
