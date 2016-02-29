f45dff278d5c5bc24d01d2ae99629ee73a63f85c
Dennis Coldwell <dennis.coldwell@gmail.com>
2016-02-28 23:26:12 -0800

    making cleanup changes and git config to linux script

--- linux/install.sh
+++ linux/install.sh
@@ -4,38 +4,21 @@
 # author: @dencold
 #
 
-#-----------------------------------------------------------------------------------
-# Cleanup
-# - removes any pre-installed dotfiles
-#-----------------------------------------------------------------------------------
-echo "- Cleaning any previous dotfiles"
-rm -f $HOME/.vimrc; rm -rf $HOME/.vim;
-rm -f $HOME/.bash_profile; rm -f $HOME/.bashrc; rm -f $HOME/.bash_prompt
-rm -f $HOME/.inputrc
-
-#-----------------------------------------------------------------------------------
-# Directory Initialization
-# - sets up our required directories
-#-----------------------------------------------------------------------------------
-echo "- Creating initial directories"
-mkdir -p $HOME/src $HOME/bin
-
-#-----------------------------------------------------------------------------------
-# bash
-# -installs .profile
-#-----------------------------------------------------------------------------------
-
 # first, figure out what directory we are in. This script is called from the main 
 # install.sh in the root directory (dotfiles). This SO article is *very* helpful:
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "- Installing .profile"
-cp -p $DIR/.bashrc $HOME/.bashrc
-cp -p $DIR/.bash_profile $HOME/.bash_profile
-cp -p $DIR/.bash_prompt $HOME/.bash_prompt
-cp -p $DIR/.inputrc $HOME/.inputrc
+echo "- Installing profiles"
+for file in .bashrc .bash_profile .bash_prompt .inputrc; do
+    cp -p $DIR/$file $HOME/$file;
+done;
 
 # finally, copy .vimrc
+echo "- Installing vim config"
 cp -p $DIR/.vimrc $HOME/.vimrc
 
+echo "- Installing git config"
+for file in .gitignore .gitconfig; do
+    cp -p $DIR/$file $HOME/$file;
+done;
