32a8ab81aac543be50f5b33bcb6fea2994a97628
Dennis Coldwell <dennis.coldwell@gmail.com>
2025-01-29 21:04:36 -0800

    adding neovim and alacritty

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,7 +13,9 @@
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ pkgs.vim
+        [
+          pkgs.neovim
+          pkgs.alacritty
         ];
 
       # Necessary for using flakes on this system.
