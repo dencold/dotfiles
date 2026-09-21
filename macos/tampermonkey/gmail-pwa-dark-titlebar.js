41a15c8c86a15b0008de28904fdec1111697296d
Dennis Coldwell <dennis.coldwell@gmail.com>
2026-09-21 11:03:50 -0700

    Update gmail-pwa-dark-titlebar.js

--- macos/tampermonkey/gmail-pwa-dark-titlebar.js
+++ macos/tampermonkey/gmail-pwa-dark-titlebar.js
@@ -6,6 +6,8 @@
 // @run-at       document-start
 // @grant        none
 // @version      0.0.3
+// @downloadURL  https://raw.githubusercontent.com/dencold/dotfiles/refs/heads/master/macos/tampermonkey/gmail-pwa-dark-titlebar.js
+// @updateURL    https://raw.githubusercontent.com/dencold/dotfiles/refs/heads/master/macos/tampermonkey/gmail-pwa-dark-titlebar.js
 // ==/UserScript==
 
 (function() {
